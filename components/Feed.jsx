bc9c4a770460ea0037068cb1bb75830b4340e5e8
adriansth <soto.herrera.adrian@gmail.com>
2022-09-10 00:37:28 -0500

    feat: Added Tweet input form.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,8 @@
 import { SparklesIcon } from '@heroicons/react/24/outline';
 
+// Components
+import Input from './Input';
+
 export default function Feed() {
     return(
         <div className='xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl'>
@@ -9,6 +12,7 @@ export default function Feed() {
                     <SparklesIcon className='h-5' />
                 </div>
             </div>
+            <Input />
         </div>
     );
 }
