86b6fbeb737fe19c6e0c80e37f8fb8d4b48421ce
adriansth <soto.herrera.adrian@gmail.com>
2022-09-11 21:20:54 -0500

    fix: Change hovering effect for Show More button at the News section.

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -21,7 +21,7 @@ export default function Widgets({ newsResults, randomUsersResults }) {
                 {newsResults.slice(0, articleNum).map((article) => (
                     <News key={article.title} article={article} />
                 ))}
-                <button onClick={() => setArticleNum(articleNum + 3)} className='text-blue-300 pl-4 pb-3 hover:bg-blue-400'>Show more</button>
+                <button onClick={() => setArticleNum(articleNum + 3)} className='text-blue-300 pl-4 pb-3 hover:text-blue-400'>Show more</button>
             </div>
             <div className='sticky top-16 text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[90%] xl:w-[75%]'>
                 <h4 className='font-bold text-xl px-4'>Who to follow</h4>
