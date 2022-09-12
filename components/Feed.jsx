0c045380af23ddcacfcc546000d23ee6f0832fbf
adriansth <soto.herrera.adrian@gmail.com>
2022-09-12 15:51:05 -0500

    feat: Tweets and posts from db complete.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,4 +1,7 @@
+import { collection, onSnapshot, orderBy, query } from '@firebase/firestore';
 import { SparklesIcon } from '@heroicons/react/24/outline';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase';
 
 // Components
 import Input from './Input';
@@ -6,26 +9,14 @@ import Post from './Post';
 
 export default function Feed() {
 
-    const posts = [
-        {
-            id: '1',
-            name: 'Adrian Soto',
-            username: 'adrianst0',
-            userImg: 'https://media-exp1.licdn.com/dms/image/C5603AQFWV8NNd6QH7A/profile-displayphoto-shrink_800_800/0/1662679020955?e=1668038400&v=beta&t=QWyNI1d_HPnCV2RArX_UBFFLrj7QCj-XMvLNkDPqCyE',
-            img: 'https://images.unsplash.com/photo-1580428180098-24b353d7e9d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
-            text: 'Nice photo!',
-            timestamp: '2 hours ago',
-        },
-        {
-            id: '2',
-            name: 'Adrian Soto',
-            username: 'adrianst0',
-            userImg: 'https://media-exp1.licdn.com/dms/image/C5603AQFWV8NNd6QH7A/profile-displayphoto-shrink_800_800/0/1662679020955?e=1668038400&v=beta&t=QWyNI1d_HPnCV2RArX_UBFFLrj7QCj-XMvLNkDPqCyE',
-            img: 'https://images.unsplash.com/photo-1627589161730-0d90bea5a656?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-            text: 'Wow',
-            timestamp: '2 days ago',
-        }
-    ];
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        onSnapshot(
+            query(collection(db, 'posts'), orderBy('timestamp', 'desc')), (snapshot) => {
+            setPosts(snapshot.docs);
+        });
+    }, []);
 
     return(
         <div className='xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[700px] md:min-w-[600px] sm:ml-[73px] flex-grow max-w-xl'>
--- components/Post.jsx
+++ components/Post.jsx
@@ -1,27 +1,30 @@
 import { EllipsisHorizontalIcon, ChatBubbleOvalLeftEllipsisIcon, TrashIcon, HeartIcon, ShareIcon, ChartBarIcon } from '@heroicons/react/24/outline';
+import Moment from 'react-moment';
 
 export default function Post({post}) {
     return(
         <div className='flex p-3 cursor-pointer border-b border-gray-200'>
             {/* User image */}
-            <img className='h-11 w-11 rounded-full mr-4' src={post.userImg} alt="user-img" />
+            <img className='h-11 w-11 rounded-full mr-4' src={post.data().userImg} alt="user-img" />
             {/* Right side */}
             <div>
                 {/* Header */}
                 <div className='flex items-center justify-between'>
                     {/* Post user info */}
                     <div className='flex items-center space-x-1 whitespace-nowrap'>
-                        <h4 className='font-bold text-[15px] sm:text-[16px] hover:underline'>{post.name}</h4>
-                        <span className='text-sm sm:text-[15px]'>@{post.username} - </span>
-                        <span className='text-sm sm:text-[15px] hover:underline'>{post.timestamp}</span>
+                        <h4 className='font-bold text-[15px] sm:text-[16px] hover:underline'>{post.data().name}</h4>
+                        <span className='text-sm sm:text-[15px]'>@{post.data().username} - </span>
+                        <span className='text-sm sm:text-[15px] hover:underline'>
+                            <Moment fromNow>{post.data().timestamp.toDate()}</Moment>
+                        </span>
                     </div>
                     {/* Dot icon */}
                     <EllipsisHorizontalIcon className='h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2' />
                 </div>
                 {/* Post text */}
-                <p className='text-gray-800 text-[15px] sm:text-[16px] mb-2'>{post.text}</p>
+                <p className='text-gray-800 text-[15px] sm:text-[16px] mb-2'>{post.data().text}</p>
                 {/* Post image */}
-                <img className='rounded-2xl mr-2' src={post.img} alt="post-img" />
+                <img className='rounded-2xl mr-2' src={post.data().image} alt="post-img" />
                 {/* Icons */}
                 <div className='flex justify-between text-gray-500 p-2'>
                     <ChatBubbleOvalLeftEllipsisIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
