410edfb157a7ba827c80017c62a2a172c39bb902
adriansth <soto.herrera.adrian@gmail.com>
2022-09-12 10:59:07 -0500

    styles: Modified responsiveness and widths for Feed and Widgets sections.

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,11 +1,39 @@
 import { useSession, signOut } from 'next-auth/react'; 
+import { useState, useRef } from 'react';
 
 import { PhotoIcon } from "@heroicons/react/24/outline";
 import { FaceSmileIcon } from "@heroicons/react/24/outline";
 
+import { db } from '../firebase';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+
 export default function Input() {
 
     const { data: session } = useSession();
+    const [input, setInput] = useState('');
+    const filePickerRef = useRef(null);
+
+    const sendPost = async () => {
+        const docRef = await addDoc(collection(db, 'posts'), {
+            id: session.user.uid,
+            text: input,
+            userImg: session.user.image,
+            timestamp: serverTimestamp(),
+            name: session.user.name,
+            username: session.user.username,
+        });
+        setInput('');
+    }
+
+    const addImageToPost = (e) => {
+        const reader = new FileReader();
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
+        reader.onload = (readerEvent) => {
+            console.log(readerEvent.target.results);
+        }
+    }
 
     return(
         <>
@@ -19,14 +47,27 @@ export default function Input() {
                     />
                     <div className='w-full divide-y divide-gray-200'>
                         <div className=''>
-                            <textarea className='w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700' rows="2" placeholder="What's happening?"></textarea>
+                            <textarea 
+                                className='w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700' 
+                                rows="2" 
+                                placeholder="What's happening?"
+                                value={input}
+                                onChange={(e) => setInput(e.target.value)}
+                            ></textarea>
                         </div>
                         <div className='flex items-center justify-between pt-2.5'>
                             <div className='flex'>
-                                <PhotoIcon className='h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100' />
+                                <div onClick={() => filePickerRef.current.click()}>
+                                    <PhotoIcon className='h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100' />
+                                    <input type="file" hidden ref={filePickerRef} onClick={addImageToPost} />
+                                </div>
                                 <FaceSmileIcon className='h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100' />
                             </div>
-                            <button className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'>Tweet</button>
+                            <button 
+                                disabled={!input.trim()} 
+                                className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'
+                                onClick={sendPost}
+                                >Tweet</button>
                         </div>
                     </div>
                 </div>
--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -9,7 +9,7 @@ export default function Widgets({ newsResults, randomUsersResults }) {
     const [userNum, setUserNum] = useState(3);
 
     return(
-        <div className='xl:w-[600px] hidden lg:inline ml-8 space-y-5'>
+        <div className='xl:w-[500px] md:w-[300px] hidden lg:inline ml-8 space-y-5'>
             <div className='w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50'>
                 <div className='flex items-center p-3 rounded-full relative'>
                     <MagnifyingGlassIcon className='h-5 z-50 text-gray-500' />
