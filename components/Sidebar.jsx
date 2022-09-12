6a9e141335eb1bf9fa7b4dce03a4774ac7fe14c9
adriansth <soto.herrera.adrian@gmail.com>
2022-09-11 21:17:36 -0500

    feat: Get session and dynamic Sidebar and Input components.

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,5 +1,6 @@
 // NextJS
 import Image from 'next/image';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 // Components
 import SidebarMenuItem from './SidebarMenuItem';
@@ -9,6 +10,9 @@ import { HomeIcon } from '@heroicons/react/24/solid';
 import { HashtagIcon, BellIcon, InboxIcon, BookmarkIcon, ClipboardIcon, UserIcon, EllipsisHorizontalCircleIcon, EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
 
 export default function Sidebar() {
+
+    const { data: session } = useSession();
+
     return(
         <div className='hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24'>
             {/* Twitter Logo */}
@@ -20,28 +24,38 @@ export default function Sidebar() {
             <div className='mt-4 mb-2.5 xl:items-start'>
                 <SidebarMenuItem text='Home' Icon={HomeIcon} active />
                 <SidebarMenuItem text='Explore' Icon={HashtagIcon} />
-                <SidebarMenuItem text='Notifications' Icon={BellIcon} />
-                <SidebarMenuItem text='Messages' Icon={InboxIcon} />
-                <SidebarMenuItem text='Bookmarks' Icon={BookmarkIcon} />
-                <SidebarMenuItem text='Lists' Icon={ClipboardIcon} />
-                <SidebarMenuItem text='Profile' Icon={UserIcon} />
-                <SidebarMenuItem text='More' Icon={EllipsisHorizontalCircleIcon} />
+                {session && (
+                    <>
+                        <SidebarMenuItem text='Notifications' Icon={BellIcon} />
+                        <SidebarMenuItem text='Messages' Icon={InboxIcon} />
+                        <SidebarMenuItem text='Bookmarks' Icon={BookmarkIcon} />
+                        <SidebarMenuItem text='Lists' Icon={ClipboardIcon} />
+                        <SidebarMenuItem text='Profile' Icon={UserIcon} />
+                        <SidebarMenuItem text='More' Icon={EllipsisHorizontalCircleIcon} />
+                    </>
+                )}
             </div>
 
-            {/* Button */}
-            <button className='bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline'>Tweet</button>
-
-            {/* Mini-Profile */}
-            <div className='hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto'>
-                <img src='https://media-exp1.licdn.com/dms/image/C5603AQFWV8NNd6QH7A/profile-displayphoto-shrink_800_800/0/1662679020955?e=1668038400&v=beta&t=QWyNI1d_HPnCV2RArX_UBFFLrj7QCj-XMvLNkDPqCyE' alt='user-img'
-                    className='h-10 w-10 rounded-full xl:mr-2'
-                />
-                <div className='leading-5 hidden xl:inline'>
-                    <h4 className='font-bold'>Adrian Soto</h4>
-                    <p className='text-gray-500'>@adrianst0</p>
-                </div>
-                <EllipsisHorizontalIcon className='h-5 xl:ml-8 hidden xl:inline' />
-            </div>
+            {session ? (
+                <>
+                    <button className='bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline'>Tweet</button>
+                    <div className='hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto'>
+                        <img 
+                        src={session.user.image}
+                        alt='user-img'
+                        className='h-10 w-10 rounded-full xl:mr-2'
+                        onClick={signOut}
+                        />
+                        <div className='leading-5 hidden xl:inline'>
+                            <h4 className='font-bold text-sm'>{session.user.name}</h4>
+                            <p className='text-gray-500 text-sm'>@{session.user.username}</p>
+                        </div>
+                        <EllipsisHorizontalIcon className='h-5 xl:ml-8 hidden xl:inline' />
+                    </div>
+                </>
+            ) : (
+                <button onClick={signIn} className='bg-blue-400 text-white rounded-full w-36 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline'>Sign in</button>
+            )}
 
         </div>
     );
