e095a92284cc66f20d22386f456d16b3dcd91f74
adriansth <soto.herrera.adrian@gmail.com>
2022-09-12 19:26:45 -0500

    feat: Managed Likes with database.

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,7 +1,39 @@
 import { EllipsisHorizontalIcon, ChatBubbleOvalLeftEllipsisIcon, TrashIcon, HeartIcon, ShareIcon, ChartBarIcon } from '@heroicons/react/24/outline';
+import { HeartIcon as HeartIconFilled } from '@heroicons/react/24/solid';
 import Moment from 'react-moment';
+import { setDoc, doc, onSnapshot, collection } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+
 
 export default function Post({post}) {
+
+    const { data: session } = useSession();
+    const [likes, setLikes] = useState([]);
+    const [hasLikes, setHasLikes] = useState(false);
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(
+            collection(db, 'posts', post.id, 'likes'),
+            (snapshot) => {
+                setLikes(snapshot.docs);
+            }
+        )
+    }, [db]);
+
+    useEffect(() => {
+        setHasLikes(likes.findIndex((like) => {
+            like.id === session.user.uid;
+        }) !== -1);
+    }, [likes]);
+
+    const likePost = async () => {
+        await setDoc(doc(db, 'posts', post.id, 'likes', session.user.uid), {
+            username: session.user.username,
+        });
+    }
+
     return(
         <div className='flex p-3 cursor-pointer border-b border-gray-200'>
             {/* User image */}
@@ -15,7 +47,7 @@ export default function Post({post}) {
                         <h4 className='font-bold text-[15px] sm:text-[16px] hover:underline'>{post.data().name}</h4>
                         <span className='text-sm sm:text-[15px]'>@{post.data().username} - </span>
                         <span className='text-sm sm:text-[15px] hover:underline'>
-                            <Moment fromNow>{post.data().timestamp.toDate()}</Moment>
+                            <Moment fromNow>{post?.data().timestamp?.toDate()}</Moment>
                         </span>
                     </div>
                     {/* Dot icon */}
@@ -29,7 +61,13 @@ export default function Post({post}) {
                 <div className='flex justify-between text-gray-500 p-2'>
                     <ChatBubbleOvalLeftEllipsisIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
                     <TrashIcon className='h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100' />
-                    <HeartIcon className='h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100' />
+
+                    {hasLikes ? (
+                        <HeartIconFilled onClick={likePost} className='h-9 w-9 hoverEffect p-2 text-red-600 hover:bg-red-100' />
+                    ) : (
+                        <HeartIcon onClick={likePost} className='h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100' />
+                    )}
+
                     <ShareIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
                     <ChartBarIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
                 </div>
